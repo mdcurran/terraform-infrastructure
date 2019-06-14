9a461014e11638a72f46efc32a5f67e5c4fbae13
Max Curran <maxcurran96@gmail.com>
2019-06-14 10:20:54 +0100

    Chore: provide basic information in the README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # terraform-infrastructure
 
+This repository details the configuration of the infrastructure I've deployed for
+personal projects on different cloud providers - currently AWS and GCP.
--- providers/gcp/variables.tf
+++ providers/gcp/variables.tf
@@ -3,7 +3,6 @@ variable "gcp_project_id" {
   description = "ID of project to deploy resources into"
 }
 
-
 variable "gcp_region" {
   default     = "europe-west2"
   description = "GCP region to deploy resources into"
@@ -18,4 +17,3 @@ variable "gcp_org_id" {
   default     = "382911368699"
   description = "The Google Cloud organisation ID to link with"
 }
-
